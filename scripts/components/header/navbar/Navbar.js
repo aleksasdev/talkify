0d21b7c3cbfe76034bd3da78351f9ce90189517c
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-05 16:03:47 +0200

    added menu

--- scripts/components/header/navbar/Navbar.js
+++ scripts/components/header/navbar/Navbar.js
@@ -7,12 +7,30 @@ export default class Navbar{
 
    render(){
       this.container = document.createElement('nav');
+      this.container.classList.add('section-style');
 
-      this.logo = document.createElement('svg');
-      this.logo.innerHTML = "../../../../assets/header/logo_shade.svg";
+      this.logo = document.createElement("img");
+      this.logo.src = "../../../../assets/header/logo_shade.svg";
+
+      this.menu = document.createElement('div');
+      this.menu.classList.add('menu');
 
       this.container.append(this.logo);
 
+      this.menu.append(this.createButton("Home"));
+      this.menu.append(this.createButton("Features"));
+      this.menu.append(this.createButton("Subscribe"));
+
+      this.cta = document.createElement('')
+
       return this.container;
+   }
+
+   createButton(text){
+      this.button = document.createElement("a");
+      this.button.classList.add('menu-button');
+      this.button.textContent = text;
+
+      this.container.append(this.button);
    }
 }
