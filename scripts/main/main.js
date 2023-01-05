204dbef8073890a5edaa0f70b1c1816e9f06b489
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-05 14:59:41 +0200

    added a way to render js components alongside html

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -1,16 +1,31 @@
 import Navbar from "../components/header/navbar/Navbar.js";
 import HeaderController from "./HeaderController.js";
 
-const currentPage = window.location.href.split("/")[1];
-console.log(currentPage);
+// Constants
+const PAGE_CURRENT = window.location.href.split("/")[3];
+const PAGE_INDEX = "";
+const PAGE_FEATURES = "features.html";
+const PAGE_SUBSCRIBE = "subscribe.html";
 
-if(!currentPage){
+function renderPage(){
    new HeaderController().append(new Navbar().render());
+
+   // Index page
+   if(PAGE_CURRENT === PAGE_INDEX){
+      
+   }
+
+   if(PAGE_CURRENT === PAGE_FEATURES){
+      
+   }
+
+   if(PAGE_CURRENT === PAGE_SUBSCRIBE){
+      
+   }
+
+   var button = document.createElement('a');
+   button.textContent = "test";
+   button.href = PAGE_FEATURES;
+   new HeaderController().append(button);
 }
-
-var button = document.createElement('a');
-button.textContent = "test";
-button.href = "lol";
-new HeaderController().append(button);
-
-fetch("/lol").then()
+renderPage();
